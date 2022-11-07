56b0af65686c9d2cc0fbcd379d8af013eee5d9f0
Duodecim00 <josem2740@gmail.com>
2022-11-07 01:30:38 -0400

    pagian de register & login/ menu con 4 secciones

--- bomb_review/src/app/app-routing.module.ts
+++ bomb_review/src/app/app-routing.module.ts
@@ -3,28 +3,26 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./screen/login/login.module').then( m => m.LoginPageModule)
   },
   {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
-  },
+    path: 'register',
+    loadChildren: () => import('./screen/register/register.module').then( m => m.RegisterPageModule)
+  }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- bomb_review/src/app/components/input/input.component.ts
+++ bomb_review/src/app/components/input/input.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
 })
-export class InputComponent {
-  @Input() type: string;
-  @Input() placeholder: string;
+export class InputComponent{
+  @Input() label: string;
+  @Input() type: 'text'; //set default type be text
 
-  constructor() { }
+  focused: boolean;
+
+  onBlur(event: any){
+    const value = event.target.value;
+
+    if(!value){
+      this.focused = false
+    }
+  }
+
 }
